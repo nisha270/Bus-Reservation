0c84354365b38e798b04b6c7771322e991c2252e
Anand Kumar <connectit2anand@gmail.com>
2023-06-15 10:50:23 +0530

    Bus Service CRUD operation

--- Backend/src/main/java/com/masai/Controller/BusController.java
+++ Backend/src/main/java/com/masai/Controller/BusController.java
@@ -1,8 +1,71 @@
 package com.masai.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.masai.Services.BusService;
+import com.masai.models.Bus;
+
+import jakarta.persistence.PersistenceUnit;
+import jakarta.validation.Valid;
 
 @Controller
 public class BusController {
+	
+	@Autowired
+	BusService busService;
+	
+	@PutMapping("/addBus")
+	public ResponseEntity<String> addBus(@Valid @RequestBody Bus bus){
+		String response = busService.addBus(bus);
+		return new ResponseEntity<String>(response,HttpStatus.CREATED);
+	}
+	
+	
+	
+	@PatchMapping("/updateBus")
+	public ResponseEntity<String> updateBus(@Valid @RequestBody Bus bus){
+		String response = busService.updateBus(bus);
+		return new ResponseEntity<String>(response,HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/deleteBus/{busId}")
+	public ResponseEntity<String> deleteBus(@Valid @PathVariable Integer busId){
+		String response = busService.deleteBus(busId);
+		return new ResponseEntity<String>(response,HttpStatus.OK);
+	}
+	
+	@GetMapping("/viewBus/{busId}")
+	public ResponseEntity<Bus> viewBus(@Valid @PathVariable Integer busId){
+		Bus bus= busService.viewBus(busId);
+		return new ResponseEntity<Bus>(bus,HttpStatus.OK);
+	}
 
+	
+	@GetMapping("/viewBusByType/{busType}")
+	public ResponseEntity<List<Bus>> viewBusByType(@PathVariable String busType){
+		List<Bus> busList = busService.viewBusByType(busType);
+		return new ResponseEntity<List<Bus>>(busList,HttpStatus.OK);
+	}
+	
+
+	@GetMapping("students/{pageNumber}/{numberOfRecords}")
+	public List<Bus> getStudentPageWiseHandler(@PathVariable("pageNumber") Integer pageNumber,
+			@PathVariable("numberOfRecords") Integer numberOfRecords){
+		return busService.getAllBusPageWise(numberOfRecords, pageNumber);
+	}
+		
+	
 }
